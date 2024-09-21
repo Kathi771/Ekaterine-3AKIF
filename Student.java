6a15448578b1bfca56883c04a88258c5e1d8146a
Kathi Hahna <hah240737@spengergasse.at>
2024-09-21 22:01:30 +0200

    Test

---
+++ Student.java
@@ -0,0 +1,30 @@
+
+public class Student
+{
+    private String name;
+    private int alter;
+    private boolean matura;
+
+    public void setName(String neuName)
+    {
+        name = neuName;
+    }
+
+    public void setAlter(int neuAlter)
+    {
+        alter = neuAlter;
+    }
+
+    public void setMatura(boolean neuMatura)
+    {
+        matura = neuMatura;
+    }
+
+    public String getName()
+    {
+        return name;
+        
+        
+        
+    }
+}
